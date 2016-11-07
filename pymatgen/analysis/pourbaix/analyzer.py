50833ed17f222c023aeec4e99db42488964d34b2
Xiaohui Qu <xqu@lbl.gov>
2016-11-07 14:07:29 -0800

    fix typo

--- pymatgen/analysis/pourbaix/analyzer.py
+++ pymatgen/analysis/pourbaix/analyzer.py
@@ -99,7 +99,7 @@ class PourbaixAnalyzer(object):
         basis_vecs = []
         on_plane_points = []
         # Create basis vectors
-        for ie, entry in self._pd.stable_entries:
+        for entry in self._pd.stable_entries:
             ie = self._pd.qhull_entries.index(entry)
             row = self._pd._qhull_data[ie]
             on_plane_points.append([0, 0, row[2]])
